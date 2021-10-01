3e1548e0528b67227d3e9ef6f79d0f9c511e071f
BroPro40 <BroPro40@users.noreply.github.com>
2021-10-01 19:16:28 +0000

    Добавьте файлы проекта.

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << " * / Calculator + -  ( 0.3 )" << endl;
+	
+	
+	double x, y;
+	
+	
+	for (char a = 0; a != 61;)
+	{
+		cin >> a;
+
+		if (a == 42) {
+			cin >> y;
+			x = x * y;
+			cout << "+\n";
+			cout << x;
+		}
+		if (a == 43) {
+			cin >> y;
+			x = x + y;
+			cout << "+\n";
+			cout << x;
+		}
+		if (a == 45) {
+			cin >> y;
+			x = x - y;
+			cout << "+\n";
+			cout << x;
+		}
+		if (a == 47) {
+			cin >> y;
+			x = x / y;
+			cout << "+\n";
+			cout << x;
+		}
+
+		cout << "the answer is = " <<x;
+	}
+
+
+	return 0;
+}
